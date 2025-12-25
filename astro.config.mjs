6930ae8d93395dac34d4f1e755a7d9b322b686b6
MoScenix <2191436583@qq.com>
2025-12-25 15:10:54 +0800

    修复界面显示问题

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://MoScenix.cn',
-  	base: '',
+	site: 'https://moscenix.cn',
+  	base: '/',
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
